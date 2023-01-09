ea4913ec0f26b4e6d1b05e38bd2d0d45fe03f647
Jim Rublee <jrubl9427@gmail.com>
2023-01-09 14:06:26 -0500

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ const app = express();
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
-const dev_db_url = "mongodb+srv://JimR:gohkIq-fushe8-hipfoj@crdata.mg0nclu.mongodb.net/local_library?retryWrites=true&w=majority";
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = "mongodb+srv://JimR:gohkIq-fushe8-hipfoj@crdata.mg0nclu.mongodb.net/local_library?retryWrites=true&w=majority";
+//const dev_db_url = "mongodb+srv://JimR:gohkIq-fushe8-hipfoj@crdata.mg0nclu.mongodb.net/local_library?retryWrites=true&w=majority";
+//const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
